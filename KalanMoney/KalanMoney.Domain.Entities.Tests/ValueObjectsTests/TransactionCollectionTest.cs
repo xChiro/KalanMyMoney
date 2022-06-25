5ef52dab2b35acf98265e5c33d8429e05d79fb9b
chiro <jorge.del1991@yahoo.com.mx>
2022-06-24 22:51:05 -0500

    Add transaction collection tests cases

---
+++ KalanMoney/KalanMoney.Domain.Entities.Tests/ValueObjectsTests/TransactionCollectionTest.cs
@@ -0,0 +1,31 @@
+using KalanMoney.Domain.Entities.ValueObjects;
+using Xunit;
+
+namespace KalanMoney.Domain.Entities.Tests.ValueObjectsTests;
+
+public class TransactionCollectionTest
+{
+    [Fact]
+    public void Create_a_new_empty_transaction_collection_successfully()
+    {
+        // Arrange/Act
+        var sut = new TransactionCollection();
+
+        // Assert
+        Assert.Empty(sut.Items);
+    }
+
+    [Fact]
+    public void Add_a_transaction_to_collection_successfully()
+    {
+        // Arrange
+        var transaction = new Transaction(105.43m);
+        var sut = new TransactionCollection();
+
+        // Act
+        sut.AddTransaction(transaction);
+
+        // Assert
+        Assert.Single(sut.Items);
+    }
+}
---
+++ KalanMoney/KalanMoney.Domain.UseCases.Tests/AddIncomeTransactionUseCaseTest.cs
@@ -0,0 +1,23 @@
+using KalanMoney.Domain.Entities;
+using KalanMoney.Domain.Entities.ValueObjects;
+using Xunit;
+
+namespace KalanMoney.Domain.UseCases.Tests;
+
+public class AddIncomeTransactionUseCaseTest
+{
+    [Fact]
+    public void Try_to_add_income_transaction_to_an_unexciting_account()
+    {
+        // Arrange
+        // var accountId = Guid.NewGuid().ToString();
+        // var name = AccountName.Create("Account Name");
+        //
+        //
+        // var account = new FinancialAccount(accountId,);
+
+        // Act
+
+        // Assert
+    }
+}
