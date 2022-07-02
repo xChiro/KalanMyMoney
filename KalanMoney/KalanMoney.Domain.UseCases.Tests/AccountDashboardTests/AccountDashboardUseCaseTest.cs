52085f19781fcb8c2c8933bde983d32a8bcf8784
chiro <jorge.del1991@yahoo.com.mx>
2022-07-01 22:05:52 -0500

    Add missin test case in account dashboard

--- KalanMoney/KalanMoney.Domain.UseCases.Tests/AccountDashboardTests/AccountDashboardUseCaseTest.cs
+++ KalanMoney/KalanMoney.Domain.UseCases.Tests/AccountDashboardTests/AccountDashboardUseCaseTest.cs
@@ -44,6 +44,40 @@ public class AccountDashboardUseCaseTest
     }
 
     [Fact]
+    public void Request_a_dashboard_with_one_income_transaction_but_without_balance_category_successfully()
+    {
+        // Arrange
+        decimal[] transactionsAmounts = { 100.10m };
+        var numberOfTransactions = transactionsAmounts.Length;
+
+        var transactions = CreateTransactions(numberOfTransactions, transactionsAmounts);
+
+        var accountQueriesRepository = new Mock<IAccountQueriesRepository>();
+        accountQueriesRepository
+            .Setup(x => x.GetTransactions(It.IsAny<string>(), It.IsAny<TransactionFilter>()))
+            .Returns(transactions);
+
+        var categoryQueriesRepository = new Mock<ICategoryQueriesRepository>();
+        categoryQueriesRepository
+            .Setup(x => x.GetCategoriesOfAccount(It.IsAny<string>(), It.IsAny<TransactionFilter>()))
+            .Returns(default(FinancialCategory[]));
+
+        var sut = new AccountDashboardUseCase(accountQueriesRepository.Object, categoryQueriesRepository.Object);
+        var output = new AccountDashboardOutputMock();
+        var accountId = Guid.NewGuid().ToString();
+        
+        // Act
+        sut.Execute(accountId, output);
+
+        // Assert
+        Assert.Equal(accountId, output.AccountDashboardRequest.AccountId);
+        Assert.NotNull(output.AccountDashboardRequest.AccountTransactions);
+        Assert.NotEmpty(output.AccountDashboardRequest.AccountTransactions);
+        Assert.Equal(transactionsAmounts[0], output.AccountDashboardRequest.AccountTransactions[0].Amount);
+        Assert.Null(output.AccountDashboardRequest.CategoryBalanceModels);
+    }
+    
+    [Fact]
     public void Request_a_dashboard_with_one_income_transaction_and_salary_balance_category_successfully()
     {
         // Arrange
